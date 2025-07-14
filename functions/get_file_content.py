5bd26b809a5e3a7aeb19dba86e09666128a80888
KazrielEversio <VarlenVentru@Yahoo.com>
2025-07-14 15:59:11 -0400

    Added "read file" functionality

---
+++ functions/get_file_content.py
@@ -0,0 +1,20 @@
+import os
+from os.path import join
+
+def get_file_content(working_directory, file_path):
+    
+    try:
+        full_path = os.path.abspath(join(working_directory, file_path))
+        if not full_path.startswith(os.path.abspath(working_directory)):
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        if not os.path.isfile(full_path):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+        MAX_CHARS = 10000
+        with open(full_path, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+            if len(file_content_string) >= MAX_CHARS:
+                return f'{file_content_string}[...File "{file_path}" truncated at 10000 characters]'
+            else:
+                return file_content_string
+    except Exception as e:
+        return f"Error: {str(e)}"
--- tests.py
+++ tests.py
@@ -1,13 +1,10 @@
-from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 print("Result for current directory:")
-print(get_files_info("calculator", "."))
+print(get_file_content("calculator", "main.py"))
 
-print("\nResult for 'pkg' directory:")
-print(get_files_info("calculator", "pkg"))
+print("Result for current directory:")
+print(get_file_content("calculator", "pkg/calculator.py"))
 
-print("\nResult for '/bin' directory:")
-print(get_files_info("calculator", '/bin'))
-
-print("\nResult for '../' directory:")
-print(get_files_info("calculator", '../'))
+print("Result for current directory:")
+print(get_file_content("calculator", "/bin/cat"))
